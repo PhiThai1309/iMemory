3c08ce43cbd5e6f5e7f456b89843f2b0d9c972b2
Phi Thai <s3878070@rmit.edu.vn>
2022-08-05 13:45:18 +0700

    Initial Commit

---
+++ iMemory/iMemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMemoryApp.swift
+//  iMemory
+//
+//  Created by Phi Thai on 05/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct iMemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
